20ae008189871c4849397cda420018670a81ea59
Ashutosh Kaushik <ashutosh.kaushik@BLRETV-C02GL7DL.local>
2022-03-14 10:33:54 +0530

    added staging url

--- locustfile.py
+++ locustfile.py
@@ -5,16 +5,18 @@ import os
 
 from locust import HttpUser, task, between
 
-SEARCH_LINK = "/product_list_v2/?q={param}&ptype=P&mtype=1&panindia=0&pincode=226002&format=2&wh=2&salt_code=&token" \
-              "=7il9iil94il9oil97il9o7l&include_discontinued=1&strict_match=1&includeGiftable=1&fallback=0" \
-              "&csrf_test_name=f4cfa96ab9aee18162f46900e4694df6&_=1645763513346 "
+# SEARCH_LINK = "/product_list_v2/?q={param}&ptype=P&mtype=1&panindia=0&pincode=226002&format=2&wh=2&salt_code=&token" \
+#               "=7il9iil94il9oil97il9o7l&include_discontinued=1&strict_match=1&includeGiftable=1&fallback=0" \
+#               "&csrf_test_name=f4cfa96ab9aee18162f46900e4694df6&_=1645763513346 "
+
+SEARCH_LINK = "/search?q={param}"
 SEARCH_QUERIES = []
 
 
 # for multiple users sending different queries
 
 class SastaSundarSearch(HttpUser):
-    host = os.getenv('TARGET_URL', 'https://search.sastasundar.com')
+    host = os.getenv('TARGET_URL', 'https://stage-search.sastasundar.com')
     wait_time = between(1, 5)
     def fetch_search_queries(self):
         files = [open("found + retail-customer + all-devices + all-scope + (2022-02-01 to 2022-02-08).csv"),
