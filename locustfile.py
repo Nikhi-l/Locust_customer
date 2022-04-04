68245c4d334f42c299e22a8da510871faf0afcf0
Ashutosh Kaushik <ashutosh.kaushik@BLRETV-C02GL7DL.local>
2022-04-04 22:11:41 +0530

    search modified

--- locustfile.py
+++ locustfile.py
@@ -5,26 +5,23 @@ import os
 
 from locust import HttpUser, task, between
 
-# SEARCH_LINK = "/product_list_v2/?q={param}&ptype=P&mtype=1&panindia=0&pincode=226002&format=2&wh=2&salt_code=&token" \
-#               "=7il9iil94il9oil97il9o7l&include_discontinued=1&strict_match=1&includeGiftable=1&fallback=0" \
-#               "&csrf_test_name=f4cfa96ab9aee18162f46900e4694df6&_=1645763513346 "
 
 SEARCH_LINK = "/search?q={param}"
-SEARCH_QUERIES = []
 
 
-# for multiple users sending different queries
+
 
 class SastaSundarSearch(HttpUser):
     host = os.getenv('TARGET_URL', 'https://search.sastasundar.com')
     wait_time = between(1, 5)
+    SEARCH_QUERIES = []
     def fetch_search_queries(self):
         files = [open("found + retail-customer + all-devices + all-scope + (2022-02-01 to 2022-02-08).csv"),
                  open("not-found + retail-customer + all-devices + all-scope + (2022-02-01 to 2022-02-08).csv")]
         for file in files:
             csv_reader = csv.reader(file)
             for name in csv_reader:
-                SEARCH_QUERIES.append(name)
+                self.SEARCH_QUERIES.append(name)
 
     def on_start(self):
         warnings.filterwarnings("ignore")
@@ -33,5 +30,4 @@ class SastaSundarSearch(HttpUser):
 
     @task
     def sasta_sundar_search_query(self):
-        self.client.get(SEARCH_LINK.format(param=random.choice(SEARCH_QUERIES)))
-        # print(response.status_code)
+        self.client.get(SEARCH_LINK.format(param=random.choice(self.SEARCH_QUERIES)))
