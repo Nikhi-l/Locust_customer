2e73b05935d829a01aebcbcf93f2355405e68d92
Ashutosh Kaushik <ashutosh.kaushik@BLRETV-C02GL7DL.local>
2022-04-06 10:50:43 +0530

    testing locust

--- locustfile.py
+++ locustfile.py
@@ -12,9 +12,10 @@ SEARCH_LINK = "/search?q={param}"
 
 
 class SastaSundarSearch(HttpUser):
-    host = os.getenv('TARGET_URL', 'https://search.sastasundar.com')
+    host = os.getenv('TARGET_URL', 'https://stage-search.sastasundar.com')
     wait_time = between(1, 5)
     SEARCH_QUERIES = []
+
     def fetch_search_queries(self):
         files = [open("found + retail-customer + all-devices + all-scope + (2022-02-01 to 2022-02-08).csv"),
                  open("not-found + retail-customer + all-devices + all-scope + (2022-02-01 to 2022-02-08).csv")]
