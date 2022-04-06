3e691fb55a5d934874bbd5dc777ff310d5235668
Ashutosh Kaushik <ashutosh.kaushik@BLRETV-C02GL7DL.local>
2022-04-06 11:54:58 +0530

    testing locust

--- locustfile_google.py
+++ locustfile_google.py
@@ -5,25 +5,15 @@ import os
 
 from locust import HttpUser, task, between
 
-SEARCH_LINK = "/?name={param}"
 
 class SastaSundarCheckout(HttpUser):
-    host = os.getenv('TARGET_URL', 'https://api.agify.io')
-    SEARCH_QUERIES = []
+    host = os.getenv('TARGET_URL', 'https://newsapi.org')
 
-    def fetch_search_queries(self):
-        files = [open("found + retail-customer + all-devices + all-scope + (2022-02-01 to 2022-02-08).csv"),
-                 open("not-found + retail-customer + all-devices + all-scope + (2022-02-01 to 2022-02-08).csv")]
-        for file in files:
-            csv_reader = csv.reader(file)
-            for name in csv_reader:
-                self.SEARCH_QUERIES.append(name)
 
     def on_start(self):
         warnings.filterwarnings("ignore")
         self.client.verify = False
-        self.fetch_search_queries()
 
     @task
     def sasta_sundar_search_query(self):
-        self.client.get(SEARCH_LINK.format(param=random.choice(self.SEARCH_QUERIES)))
+        self.client.get("/v2/top-headlines?country=us&category=business&apiKey=eb87d9b1b57d4c25a6fa5049d2014794")
