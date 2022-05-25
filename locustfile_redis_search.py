da53920659812d472371004bebcf709b1aef2e90
Ashutosh-SS <ashutosh.kaushik@flipkart.com>
2022-05-25 19:31:19 +0530

    wait time removed

--- locustfile_redis_search.py
+++ locustfile_redis_search.py
@@ -8,7 +8,6 @@ SEARCH_LINK = ""
 
 class SastaSundarSearch(HttpUser):
     host = os.getenv('TARGET_URL', 'https://search.sastasundar.com/search_reserved_keyword?q=best-deal&aggregated=1&token=&device=5&wh=1&panindia=0&pincode=700102&exclude_items=1&page=2&size=1')
-    wait_time = between(1, 5)
 
     def on_start(self):
         warnings.filterwarnings("ignore")
