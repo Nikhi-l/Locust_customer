ff3581db8669988071c6647bfbc32cfb36974fc3
Ashutosh-SS <ashutosh.kaushik@flipkart.com>
2022-05-30 21:59:49 +0530

    blank locust

---
+++ locustfile_blank.py
@@ -0,0 +1,18 @@
+import csv
+import random
+import warnings
+import os
+
+from locust import HttpUser, task, between
+
+
+class SastaSundarSearch(HttpUser):
+    host = os.getenv('TARGET_URL', '')
+
+    def on_start(self):
+        warnings.filterwarnings("ignore")
+        self.client.verify = False
+
+    @task
+    def sasta_sundar_search_query(self):
+        self.client.get("/")
