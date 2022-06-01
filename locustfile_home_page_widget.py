1c5842e46a79c138d93bc68df681c3d27f4fee2c
Ashutosh-SS <ashutosh.kaushik@flipkart.com>
2022-06-01 18:40:10 +0530

    home page widget configured

--- locustfile_home_page_widget.py
+++ locustfile_home_page_widget.py
@@ -6,26 +6,20 @@ import os
 from locust import FastHttpUser, task, between
 
 body = {
-  "page":1,
-  "panindia":0,
-  "warehouseId":"1",
-  "pincode":700120,
-  "userId":"4937724",
-  "profileId":"",
-  "app_type":"N",
-  "app_version":"4.0.4",
-  "app_version_code":109,
-  "resolution_type":"xhdpi",
-  "deviceId":"81653dce-0dd2-4201-8916-4aecbdd89269",
-  "IsLab":1
+    "page": 1,
+    "panindia": 0,
+    "warehouseId": "1",
+    "pincode": 700120,
+    "userId": "4937724",
+    "profileId": "",
+    "app_type": "N",
+    "app_version": "4.0.4",
+    "app_version_code": 109,
+    "resolution_type": "xhdpi",
+    "deviceId": "81653dce-0dd2-4201-8916-4aecbdd89269",
+    "IsLab": 1
 }
 
-header = {
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Content-Length": "198",
-    "Accept-Encoding": "gzip, deflate",
-    "User-Agent": "okhttp/5.0.0-alpha.2"
-}
 
 class SastaSundarCheckout(FastHttpUser):
     host = os.getenv('TARGET_URL', 'https://catalog.sastasundar.com')
@@ -36,5 +30,4 @@ class SastaSundarCheckout(FastHttpUser):
 
     @task
     def sasta_sundar_search_query(self):
-        self.client.post("/home/getmasterhomewidgets", headers=header, data=body)
-        self.client.cookies.clear()
+        self.client.post("/home/getmasterhomewidgets", json=body)
