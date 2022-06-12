cca2ed7bd9d16c6e3b22c1e2ce778d06f13f8fcc
Ashutosh-SS <ashutosh.kaushik@flipkart.com>
2022-06-12 12:37:56 +0530

    exception fixed

--- locustfile_auto_login.py
+++ locustfile_auto_login.py
@@ -37,11 +37,14 @@ class SastaSundarAddToCart(HttpUser):
         with self.client.post("/index.php/webapi/fkh_app_in_app/AutoLogin_LoadTesting", catch_response=True,
                               data=body) as response:
             print(response.text)
-            json_response = response.json()
-            if response.status_code == 200:
-                if json_response.get("data", {}).get("data", {}).get("UserId", "null") != "null":
-                    response.success()
+            try:
+                json_response = response.json()
+                if response.status_code == 200:
+                    if json_response.get("data", {}).get("data", {}).get("UserId", "null") != "null":
+                        response.success()
+                    else:
+                        response.failure(f'UserId not received in response!')
                 else:
-                    response.failure(f'UserId not received in response!')
-            else:
-                response.failure(f'status code {response.status_code}')
+                    response.failure(f'status code {response.status_code}')
+            except:
+                pass
